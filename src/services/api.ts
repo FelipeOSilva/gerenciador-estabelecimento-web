070abce2f52d6f4bf8a53e14b44e68a94f6f35c3
Felipe Silva <felipe.silva@teclat.com.br>
2020-11-27 08:19:49 -0300

    feat: para que todas requisições enviem o token

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,13 @@ const api = axios.create({
   baseURL: 'http://localhost:3333'
 })
 
+api.interceptors.request.use(
+  async (config) => {
+    const token = sessionStorage.getItem('jwt_token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  }
+)
 export default api
